3200515ebe054ffc6aba6bb3f761ed696d1ab77d
Teun Stout <60847859+teunstout@users.noreply.github.com>
2020-04-18 10:51:10 +0200

    Let roomdatabase make an empty note when empty

--- Notepad/app/src/main/java/com/example/notepad/database/NoteDatabase.kt
+++ Notepad/app/src/main/java/com/example/notepad/database/NoteDatabase.kt
@@ -5,12 +5,17 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.notepad.model.DateConverter
 import com.example.notepad.model.Note
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.*
 
 @TypeConverters(DateConverter::class)
 @Database(entities = [Note::class], version = 1, exportSchema = false)
-abstract class NoteDatabase: RoomDatabase() {
+abstract class NoteDatabase : RoomDatabase() {
 
     abstract fun noteDao(): NoteInterface // Use the interface to make the note app
 
@@ -23,7 +28,8 @@ abstract class NoteDatabase: RoomDatabase() {
 
         fun getDatabase(context: Context): NoteDatabase? {
             if (INSTANCE == null) {
-                synchronized(NoteDatabase::class.java) { // make database synchronized so that data is not edited at the same time. Data won't go faulty now.
+                synchronized(NoteDatabase::class.java) {
+                    // make database synchronized so that data is not edited at the same time. Data won't go faulty now.
                     if (INSTANCE == null) {
                         INSTANCE = Room.databaseBuilder( // if there is no instance make a instance
                             context.applicationContext,
@@ -31,6 +37,18 @@ abstract class NoteDatabase: RoomDatabase() {
                             DATABASE_NAME
                         )
                             .fallbackToDestructiveMigration()
+                            .addCallback(object :
+                                RoomDatabase.Callback() { // Empty note needs to be created with a new database
+                                override fun onCreate(db: SupportSQLiteDatabase) { // overwrite the oncreate
+                                    super.onCreate(db)
+                                    INSTANCE?.let { database -> // If instance is null make a Date
+                                        CoroutineScope(Dispatchers.IO).launch { // Launch Corountine
+                                            database.noteDao() // Call the database it's interface
+                                                .insertNote(Note("Title", Date(), "")) // Insert empty note
+                                        }
+                                    }
+                                }
+                            })
                             .build()
                     }
                 }
@@ -38,8 +56,6 @@ abstract class NoteDatabase: RoomDatabase() {
             return INSTANCE
         }
     }
-
-
 
 
 }
