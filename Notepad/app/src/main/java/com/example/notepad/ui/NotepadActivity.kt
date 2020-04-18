3a56ff3055fb60c9c89d298e4f8e130646929e75
Teun Stout <60847859+teunstout@users.noreply.github.com>
2020-04-18 11:13:09 +0200

    4.2

--- Notepad/app/src/main/java/com/example/notepad/ui/NotepadActivity.kt
+++ Notepad/app/src/main/java/com/example/notepad/ui/NotepadActivity.kt
@@ -1,22 +1,51 @@
 package com.example.notepad.ui
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.observe
 import com.example.notepad.R
+import com.example.notepad.viewmodel.NoteViewholder
 
 import kotlinx.android.synthetic.main.activity_notepad.*
+import kotlinx.android.synthetic.main.content_edit_notepad.*
+import kotlinx.android.synthetic.main.content_notepad.*
+import java.util.Observer
 
 class NotepadActivity : AppCompatActivity() {
+
+    //    private lateinit var noteViewholder: NoteViewholder
+    private val noteViewholder: NoteViewholder by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notepad)
         setSupportActionBar(toolbar)
 
+        initViews()
+        initViewModel()
+
+    }
+
+    private fun initViews() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
     }
+
+    private fun initViewModel() {
+//        noteViewholder = ViewModelProvider(this).get(NoteViewholder::class.java) // Instantiate the note viewholder
+
+
+        noteViewholder.note.observe(this, Observer { note ->
+            if (note != null) {
+                tvTitle.text = note.noteTitle
+                tvLastUpdated.text = getString(R.string.last_updated, note.lastUpdated.toString())
+                tvNote.text = note.noteText
+            }
+        })
+    }
+
 }
--- Notepad/app/src/main/java/com/example/notepad/viewmodel/NoteViewholder.kt
+++ Notepad/app/src/main/java/com/example/notepad/viewmodel/NoteViewholder.kt
@@ -1,4 +1,16 @@
 package com.example.notepad.viewmodel
 
-class NoteViewholder {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.notepad.database.NoteRepository
+
+/**
+ * All the business logic of Notepad Activity
+ */
+class NoteViewholder(application: Application): AndroidViewModel(application) {
+
+    private val noteRepository = NoteRepository(application.applicationContext) // Get note repository from application
+    val note = noteRepository.getNotepad() // Get live data from the repository
+
+
 }
